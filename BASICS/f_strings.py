9caffaf714d4cafea98ecce617a7e55f3a55108c
Dhinakar S <dhinakarshanmugam6@gmail.com>
2024-03-14 07:45:35 +0530

    CHANGED

---
+++ BASICS/f_strings.py
@@ -0,0 +1,7 @@
+name = "dhinakar"
+age = 20
+degree ="EEE"
+print(f"My name is {name}. I am {age} years now and I am currenty pursuing my {degree} degree.") #f-strings
+print(f"{degree*2}") #f-strings with operations also possible
+
+# print("My name is "+name+". I am "+str(age)+" years now and I am currently pursuing my "+degree+" degree.") #normal string concatenation
