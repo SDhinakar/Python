5b7b8269976104860b768639a167975b9da46176
Dhinakar S <dhinakarshanmugam6@gmail.com>
2024-03-20 18:50:14 +0530

    .

---
+++ LOOPS/control_statements.py
@@ -0,0 +1,35 @@
+# # the control statements are used to control the flow of the program and they are of 3 types: break, continue, pass .
+
+# count=1
+# while count<=10:
+#     print(count)
+#     count+=1
+#     if count==7:
+#         break
+#         # continue
+#         # pass
+#     print("Done without interruption")
+# print("Done")
+
+# list1=['hi','hello','GG']
+# list2=['dhina','vikram','siva']
+# for i in list1:
+#     for j in list2:
+#         print(i,j)
+#         if i=='hello' and j=='dhina':
+#             # continue
+#             break
+#     print("Done")
+# print("Out of the loop")
+
+# for i in range(0,11):
+#     if(i%2==0):
+#         continue
+#     else:
+#         print(i)
+
+for i in range(1,10):
+    pass
+
+def func1():
+    pass
