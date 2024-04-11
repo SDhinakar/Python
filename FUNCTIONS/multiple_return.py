1582089eee97f51cff60ada893aae94aedf2b344
Dhinakar S <dhinakarshanmugam6@gmail.com>
2024-04-11 09:33:28 +0530

    11.4.23

--- FUNCTIONS/multiple_return.py
+++ FUNCTIONS/multiple_return.py
@@ -1,10 +1,10 @@
-# import statistics
-# import math
-# def mean_median_mode(list1):
-#     return [statistics.mean(list1),statistics.median(list1),statistics.mode(list1)]
+import statistics
+import math
+def mean_median_mode(list1):
+    return [statistics.mean(list1),statistics.median(list1),statistics.mode(list1)]
 
-# a,b,c=mean_median_mode([3,5,45,3,2,1,89])
-# print(f"Mean is : {a}\nMedian is : {b}\nMode is : {c}")
+a,b,c=mean_median_mode([3,5,45,3,2,1,89])
+print(f"Mean is : {a}\nMedian is : {b}\nMode is : {c}")
 
 
 # def add(a,b):
@@ -15,10 +15,10 @@
     
 # print(add(int(input("Enter the first number : ")),int(input("Enter the second number : "))))
 
-def add(a, b):
-    if not a and not b:
-        return "Both are empty"
-    else:
-        return a + b
+# def add(a, b):
+#     if not a and not b:
+#         return "Both are empty"
+#     else:
+#         return a + b
     
-print(add(input("Enter the first number : "), input("Enter the second number : ")))
+# print(add(input("Enter the first number : "), input("Enter the second number : ")))
